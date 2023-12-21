c805f4edd1a3a4622390f3674a4c3b94dd86d249
x-yang1021 <95700029+x-yang1021@users.noreply.github.com>
2023-12-21 22:21:00 +0000

    test of cacum tutorial

--- Test/test explainable AI.py
+++ Test/test explainable AI.py
@@ -13,6 +13,9 @@ from imitation.rewards.reward_nets import BasicShapedRewardNet
 from imitation.util.networks import RunningNorm
 from imitation.util.util import make_vec_env
 
+from captum.attr import LayerConductance, LayerActivation, LayerIntegratedGradients
+from captum.attr import IntegratedGradients, DeepLift, GradientShap, NoiseTunnel, FeatureAblation
+
 SEED = 42
 
 env = make_vec_env(
@@ -34,4 +37,76 @@ rollouts = rollout.rollout(
     rng=np.random.default_rng(SEED),
 )
 
+X_Test = []
 
+for i in range(len(rollouts)):
+    for j in rollouts[i]:
+        X_Test.append(j)
+
+model = torch.load('Reward/experiment.pt')
+
+
+ig = IntegratedGradients(model)
+ig_nt = NoiseTunnel(ig)
+dl = DeepLift(model)
+gs = GradientShap(model)
+fa = FeatureAblation(model)
+
+ig_attr_test = ig.attribute(X_test, n_steps=50)
+ig_nt_attr_test = ig_nt.attribute(X_test)
+dl_attr_test = dl.attribute(X_test)
+gs_attr_test = gs.attribute(X_test, X_train)
+fa_attr_test = fa.attribute(X_test)
+
+# prepare attributions for visualization
+
+x_axis_data = np.arange(X_test.shape[1])
+x_axis_data_labels = ['Position', 'Velocity','Angle', 'Angular']
+
+ig_attr_test_sum = ig_attr_test.detach().numpy().sum(0)
+ig_attr_test_norm_sum = ig_attr_test_sum / np.linalg.norm(ig_attr_test_sum, ord=1)
+
+ig_nt_attr_test_sum = ig_nt_attr_test.detach().numpy().sum(0)
+ig_nt_attr_test_norm_sum = ig_nt_attr_test_sum / np.linalg.norm(ig_nt_attr_test_sum, ord=1)
+
+dl_attr_test_sum = dl_attr_test.detach().numpy().sum(0)
+dl_attr_test_norm_sum = dl_attr_test_sum / np.linalg.norm(dl_attr_test_sum, ord=1)
+
+gs_attr_test_sum = gs_attr_test.detach().numpy().sum(0)
+gs_attr_test_norm_sum = gs_attr_test_sum / np.linalg.norm(gs_attr_test_sum, ord=1)
+
+fa_attr_test_sum = fa_attr_test.detach().numpy().sum(0)
+fa_attr_test_norm_sum = fa_attr_test_sum / np.linalg.norm(fa_attr_test_sum, ord=1)
+
+lin_weight = model.lin1.weight[0].detach().numpy()
+y_axis_lin_weight = lin_weight / np.linalg.norm(lin_weight, ord=1)
+
+width = 0.14
+legends = ['Int Grads', 'Int Grads w/SmoothGrad','DeepLift', 'GradientSHAP', 'Feature Ablation', 'Weights']
+
+plt.figure(figsize=(20, 10))
+
+ax = plt.subplot()
+ax.set_title('Comparing input feature importances across multiple algorithms and learned weights')
+ax.set_ylabel('Attributions')
+
+FONT_SIZE = 16
+plt.rc('font', size=FONT_SIZE)            # fontsize of the text sizes
+plt.rc('axes', titlesize=FONT_SIZE)       # fontsize of the axes title
+plt.rc('axes', labelsize=FONT_SIZE)       # fontsize of the x and y labels
+plt.rc('legend', fontsize=FONT_SIZE - 4)  # fontsize of the legend
+
+ax.bar(x_axis_data, ig_attr_test_norm_sum, width, align='center', alpha=0.8, color='#eb5e7c')
+ax.bar(x_axis_data + width, ig_nt_attr_test_norm_sum, width, align='center', alpha=0.7, color='#A90000')
+ax.bar(x_axis_data + 2 * width, dl_attr_test_norm_sum, width, align='center', alpha=0.6, color='#34b8e0')
+ax.bar(x_axis_data + 3 * width, gs_attr_test_norm_sum, width, align='center',  alpha=0.8, color='#4260f5')
+ax.bar(x_axis_data + 4 * width, fa_attr_test_norm_sum, width, align='center', alpha=1.0, color='#49ba81')
+ax.bar(x_axis_data + 5 * width, y_axis_lin_weight, width, align='center', alpha=1.0, color='grey')
+ax.autoscale_view()
+plt.tight_layout()
+
+ax.set_xticks(x_axis_data + 0.5)
+ax.set_xticklabels(x_axis_data_labels)
+
+plt.legend(legends, loc=3)
+plt.show()
