9f0b6edb66d1f20922ff7ad1922c0b7219a451f8
x-yang1021 <xiangmin.yang1021@gmail.com>
2024-04-18 22:37:48 +0100

    save update for env

--- Data/Data preprocess.py
+++ Data/Data preprocess.py
@@ -8,7 +8,7 @@ import numpy as np
 import seaborn as sns
 
 # Get a list of all CSV files in a directory
-csv_files = glob.glob('./raw data 2/*.csv')
+csv_files = glob.glob('./raw data 3/*.csv')
 geod = Geod(ellps="WGS84")
 transformer = pyproj.Transformer.from_crs("EPSG:4326", "EPSG:32650", always_xy=True)
 sun = [118.77886076,32.04382648]
@@ -37,67 +37,80 @@ trajectory = pd.DataFrame()
 #convert to XY coordinates
 for csv_file in csv_files:
     df = pd.read_csv(csv_file)
-
+    df['Speed'] = np.nan
+    df['Direction'] = np.nan
     for i in range(len(df)):
         Coordinate = np.array(transformer.transform(df.iloc[i][1], df.iloc[i][2])) - sun
         df.iloc[i,1] = Coordinate[0]
         df.iloc[i,2] = Coordinate[1]
+        if i == 0:
+            df.iloc[i,3] = 0
+            df.iloc[i,4] = np.pi/2
+        else:
+            df.iloc[i, 3] = np.sqrt((df.iloc[i,1] - df.iloc[i-1,1])**2 + (df.iloc[i,2] - df.iloc[i-1,2])**2)
+            df.iloc[i, 4] = np.arctan2((df.iloc[i,2] - df.iloc[i-1,2]),(df.iloc[i,1] - df.iloc[i-1,1]))
     trajectory = pd.concat([trajectory, df], axis=1)
 
-trajectory.to_csv('./raw data 2.csv', index=False)
+with pd.ExcelWriter('./raw data.xlsx', mode='a') as writer:
+    trajectory.to_excel(writer, sheet_name= 'Group 3')
 
-useful_data = 0
 
-for j in range(0, trajectory.shape[1], 3):
-    df = trajectory.iloc[:,j:j+3]
+for j in range(0, trajectory.shape[1], 5):
+    df = trajectory.iloc[:,j:j+5]
     for i in range(len(df)):
         coordinate = df.iloc[i]
-        # if coordinate[1] > edge_start1[0] or coordinate[1] < green_start1[0]:
-        #     # print(edge_start1[0], green_start1[0], edge_start1[1], Coordinate)
-        #     continue
+        if coordinate[1] < green_start1[0]:
+            # coordinate[1] > edge_start1[0]
+            # print(edge_start1[0], green_start1[0], edge_start1[1], Coordinate)
+            trajectory.iloc[i:i+1,j:j+5] = np.nan
         if (coordinate[2] < edge_start1[1]) or (coordinate[2] > wall_end1[1] and coordinate[2] < wall_start2[1]) or (coordinate[2] > wall_end2[1]):
             # if Coordinate[0] > edge_end1[0]:
             #     if edge1(Coordinate[0]) < Coordinate[1]:
             #         print(edge1(Coordinate[0]), Coordinate[1], edge_end1[0], Coordinate[0])
             #         continue
-            trajectory.iloc[i:i+1,j:j+3] = np.nan
+            trajectory.iloc[i:i+1,j:j+5] = np.nan
+        if coordinate[3] > 2 or coordinate[3] == 0:
+            trajectory.iloc[i:i + 1, j:j + 5] = np.nan
 
-
-extra_data = []
-for i in range(0, trajectory.shape[1], 3):
-    new_trajectory = trajectory.iloc[:,i:i+3]
-    consecutive = 0
-    move = {}
-    longer_step = 0
-    useful_data += len(new_trajectory)
-    for j in range(1, len(new_trajectory)):
-        if new_trajectory.iloc[j,0] - new_trajectory.iloc[j-1,0] > 1:
-            extra_data.append(consecutive%5)
-            consecutive = 0
-        else:
-            consecutive += 1
-        if consecutive:
-            dist = np.sqrt((new_trajectory.iloc[j,1] - new_trajectory.iloc[j-1,1])**2 + (new_trajectory.iloc[j,2] - new_trajectory.iloc[j-1,2])**2)
-            if dist > 1.5:
-                longer_step += 1
-    #     move[j] = dist
-    # keys = list(move.keys())
-    # # get values in the same order as keys, and parse percentage values
-    # vals = [float(move[k]) for k in keys]
-    # sns_plot = sns.lineplot(x=keys, y=vals)
-    # fig = sns_plot.get_figure()
-    # fig.savefig("output %d.png"%i)
-    print(longer_step)
-
-
-print('need to remove:', extra_data)
-
-print('useful data:', useful_data)
-# trajectory.to_csv('./processed data.csv', index=False)
-
-
+with pd.ExcelWriter('./processed data.xlsx', mode='a') as writer:
+    trajectory.to_excel(writer, sheet_name= 'Group 3')
 
 exit()
+# useful_data = pd.DataFrame()
+# for i in range(0, trajectory.shape[1], 3):
+#     new_trajectory = trajectory.iloc[:,i:i+3]
+#     consecutive = 0
+#     prev = 0
+#     for j in range(1, len(new_trajectory)):
+#         if not pd.isna(new_trajectory.iloc[j,0]):
+#             useful_data += 1
+#             if new_trajectory.iloc[j,0] - prev > 1:
+#                 # extra_data.append(consecutive%5)
+#                 consecutive = 0
+#             else:
+#                 consecutive += 1
+#             if consecutive:
+#                 dist = np.sqrt((new_trajectory.iloc[j,1] - new_trajectory.iloc[j-1,1])**2 + (new_trajectory.iloc[j,2] - new_trajectory.iloc[j-1,2])**2)
+#                 if dist > 2:
+#                     new_trajectory.iloc[j,:] = np.nan
+#             prev = new_trajectory.iloc[j, 0]
+#         else:
+#             continue
+#     useful_data = pd.concat([useful_data, new_trajectory], axis=1)
+#     #     move[j] = dist
+#     # keys = list(move.keys())
+#     # # get values in the same order as keys, and parse percentage values
+#     # vals = [float(move[k]) for k in keys]
+#     # sns_plot = sns.lineplot(x=keys, y=vals)
+#     # fig = sns_plot.get_figure()
+#     # fig.savefig("output %d.png"%i)
+#
+# with pd.ExcelWriter('./processed data.xlsx', mode='a') as writer:
+#     useful_data.to_excel(writer, sheet_name= 'Group 2')
+
+
+
+
 
 # trajectory.to_csv('dataset.csv', index=False)
 
